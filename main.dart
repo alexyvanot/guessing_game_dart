931c6ae47a844e114a2f276e278d2106d870a792
VANOT Alexy <alexystefen@hotmail.com>
2024-09-26 12:42:15 +0200

    english + difficulty

--- main.dart
+++ main.dart
@@ -1,45 +1,83 @@
 import 'dart:io';
 import 'dart:math';
 
+enum Difficulty {
+  EASY,
+  MEDIUM,
+  HARD,  
+}
+
 void main() {
   var random = Random();
-  int targetNumber = random.nextInt(100) + 1;
+  int? targetNumber;
   int? guess;
   int attempts = 0;
   Set<int> numTried = {};
   DateTime? start;
+  Difficulty? difficulty;
 
-  print('Bienvenue dans le jeu "Guess the Number" !');
-  print('Je pense à un nombre entre 1 et 100. Pouvez-vous le deviner ?');
+  print('Welcome to "Guess the Number" !');
+
+  print("Choose your difficulty:");
+  do {
+    print("1. Easy");
+    print("2. Medium");
+    print("3. Hard");
+    stdout.write("> ");
+    int choice = int.parse(stdin.readLineSync()!);
+    switch (choice) {
+      case 1:
+        difficulty = Difficulty.EASY;
+        targetNumber = random.nextInt(100) + 1;
+        break;
+      case 2:
+        difficulty = Difficulty.MEDIUM;
+        targetNumber = random.nextInt(500) + 1;
+        break;
+      case 3:
+        difficulty = Difficulty.HARD;
+        targetNumber = random.nextInt(1000) + 1;
+        break;
+      default:
+        print("ERR: Please enter a valid choice.");
+        continue;
+    }
+  } while (targetNumber == null);
+  if (difficulty == Difficulty.EASY) {
+    print('You have chosen the easy mode. I am thinking of a number between 1 and 100. Try to find it !');
+  } else if (difficulty == Difficulty.MEDIUM) {
+    print('You have chosen the medium mode. I am thinking of a number between 1 and 500. Try to find it !');
+  } else {
+    print('You have chosen the hard mode. I am thinking of a number between 1 and 1000. Try to find it !');
+  }
 
   do {
-    stdout.write('Entrez votre estimation: ');
+    stdout.write('> ');
     try {
       guess = int.parse(stdin.readLineSync()!);
     } catch (e) {
-      print('ERR: Veuillez entrer un nombre valide.');
+      print('ERR: Please enter a valid number.');
       continue;
     }
     if (numTried.contains(guess)) {
-      print('Vous avez déjà essayé ce nombre... Essayez un autre !');
+      print('You have already tried this number. Try another one.');
       continue;
     }
     attempts++;
     numTried.add(guess);
     if (start == null) {
       start = DateTime.now();
-      print("Début du chrono " + start.toString());
+      print("START: " + start.toString());
     }
 
     if (guess < targetNumber) {
-      print('C\'est plus grand !');
+      print('The number is higher.');
     } else if (guess > targetNumber) {
-      print('C\'est plus petit !');
+      print('The number is lower.');
     } else {
       print(
-          "Congrats! You've found the number $targetNumber in $attempts attempts in ${DateTime.now().difference(start).inSeconds} seconds.");
-      print(DateTime.now().toString());
+          "Congrats! You've found the number $targetNumber in $attempts attempts in ${DateTime.now().difference(start).inSeconds} seconds with the difficulty ${difficulty.toString().split('.').last.toLowerCase()}.");
+      print("END: " + DateTime.now().toString());
     }
-    
   } while (guess != targetNumber);
 }
